c49db51fc3564113ea0cca6f5cc561e1067a3401
Jackson Valdez <jacksonvaldez64@gmail.com>
2021-11-13 15:43:29 -0700

    Begin enigma class

---
+++ lib/enigma.rb
@@ -0,0 +1,13 @@
+require 'date'
+
+class Enigma
+
+  def self.encrypt(plaintext, key, date)
+
+  end
+
+  def self.decrypt(ciphertext, key, date)
+
+  end
+
+end
---
+++ spec/enigma_spec.rb
@@ -0,0 +1,5 @@
+require './lib/enigma'
+
+describe Enigma do
+
+end
