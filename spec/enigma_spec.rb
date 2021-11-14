4f2ddcc65b8e6a13af3dd93c95e49a918163a718
Jackson Valdez <jacksonvaldez64@gmail.com>
2021-11-13 19:11:35 -0700

    Add tests for interaction pattern)

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -3,4 +3,36 @@ require 'spec_helper'
 
 describe Enigma do
 
+  before(:each) do
+    @enigma = Enigma.new
+  end
+
+  describe '#initialize' do
+    it 'has attributes' do
+      expect(@enigma.char_set).to eq(("a".."z").to_a << " ")
+    end
+  end
+
+  describe '#encrypt' do
+    it 'returns a hash with cipher text, key, and date' do
+      expected = {
+        encryption: "keder ohulw",
+        key: "02715",
+        date: "040895"
+      }
+      expect(@enigma.encrypt("hello world", "02715", "040895")).to eq(expected)
+    end
+  end
+
+  describe '#decrypt' do
+    it 'returns a hash with plain text, key, and date' do
+      expected = {
+        decryption: "hello world",
+        key: "02715",
+        date: "040895"
+      }
+      expect(@enigma.decrypt("keder ohulw", "02715", "040895")).to eq(expected)
+    end
+  end
+
 end
