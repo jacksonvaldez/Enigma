09baedd119d1b27f272eeb585a16ff94fe1d5477
Jackson Valdez <jacksonvaldez64@gmail.com>
2021-11-14 01:59:22 -0700

    Add crack test

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -8,12 +8,6 @@ describe Enigma do
     @enigma = Enigma.new
   end
 
-  describe '#initialize' do
-    it 'has attributes' do
-      expect(@enigma.char_set).to eq(("a".."z").to_a << " ")
-    end
-  end
-
   describe '#encrypt' do
     it 'returns a hash with cipher text, key, and date' do
       expected = {
@@ -33,6 +27,18 @@ describe Enigma do
         date: "040895"
       }
       expect(@enigma.decrypt("keder ohulw!tlmsvrb", "02715", "040895")).to eq(expected)
+    end
+  end
+
+  describe '#crack' do
+    it 'returns cracked decryption' do
+      expected = {
+        decryption: "hello world end",
+        date: "291018",
+        key: "08304"
+      }
+      expect(@enigma.crack("vjqtbeaweqihssi", "291018")).to eq(expected)
+      expect(@enigma.crack("vjqtbeaweqihssi")).to eq(expected)
     end
   end
 
