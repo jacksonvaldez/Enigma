d22acd3b61511313822210af718933874bc60979
Jackson Valdez <jacksonvaldez64@gmail.com>
2021-11-13 19:48:34 -0700

    Add shifts test

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -14,7 +14,7 @@ describe Enigma do
   end
 
   describe '#encrypt' do
-    it 'returns a hash with cipher text, key, and date' do
+    xit 'returns a hash with cipher text, key, and date' do
       expected = {
         encryption: "keder ohulw",
         key: "02715",
@@ -25,13 +25,19 @@ describe Enigma do
   end
 
   describe '#decrypt' do
-    it 'returns a hash with plain text, key, and date' do
+    xit 'returns a hash with plain text, key, and date' do
       expected = {
         decryption: "hello world",
         key: "02715",
         date: "040895"
       }
       expect(@enigma.decrypt("keder ohulw", "02715", "040895")).to eq(expected)
+    end
+  end
+
+  describe '#shifts' do
+    it 'returns an array of the shifts from given ket and date' do
+      expect(@enigma.shifts("02715", "040895")).to eq([3, 27, 73, 20])
     end
   end
 
