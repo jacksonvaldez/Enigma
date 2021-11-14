0893e5aa81cf27c99312295aa15f4e455c0b1fb7
Jackson Valdez <jacksonvaldez64@gmail.com>
2021-11-13 21:47:07 -0700

    Refactor shifts with better variable names

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -51,10 +51,10 @@ class Enigma
   end
 
   def shifts(key, date)
-    shifts = key.chars.each_cons(2).map { |char_x, char_y| (char_x + char_y).to_i }
-    shifts_add = date.to_i.pow(2).to_s.chars[-4..-1].map { |e| e.to_i }
-    shifts.each_with_index.map do |element, index|
-      element + shifts_add[index]
+    keys = key.chars.each_cons(2).map { |char_x, char_y| (char_x + char_y).to_i }
+    offsets = date.to_i.pow(2).to_s.chars[-4..-1].map { |e| e.to_i }
+    shifts = keys.each_with_index.map do |element, index|
+      element + offsets[index]
     end
   end
 
